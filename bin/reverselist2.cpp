bdf43c183d8fb93efa9b89942ea97a6af1f936aa
Barun Singhania <emailbarung@gmail.com>
2015-06-19 19:43:21 +0530

    Correct solution to reverse list :P

---
+++ bin/reverselist2.cpp
@@ -0,0 +1,25 @@
+everse a linked list and return pointer to the head
+  The input list will have at least one element  
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* Reverse(Node *head)
+{
+  // Complete this method
+    Node* temp=NULL;
+    Node* temp1=head;
+    Node* temp2=new Node();
+    while(temp1!=NULL)
+        {
+        temp2=temp1->next;
+        temp1->next=temp;
+        temp=temp1;
+        temp1=temp2;
+    }
+    head=temp;
+    return head;
+}
