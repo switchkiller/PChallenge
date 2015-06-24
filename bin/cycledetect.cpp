0db723530d370ae936e311dd0e4d113a49ca5168
switchkiller <emailbarung@gmail.com>
2015-06-24 20:21:56 +0545

    Detecting the loop by floyd method

---
+++ bin/cycledetect.cpp
@@ -0,0 +1,27 @@
+
+op in a linked list 
+  List could be empty also
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+int HasCycle(Node* head)
+{
+   // Complete this function
+   // Do not write the main method
+  struct Node  *slow_p = head, *fast_p = head;
+ 
+  while(slow_p && fast_p && fast_p->next )
+  {
+    slow_p = slow_p->next;
+    fast_p  = fast_p->next->next;
+    if (slow_p == fast_p)
+    {
+       return 1;
+    }
+  }
+  return 0;
+}
