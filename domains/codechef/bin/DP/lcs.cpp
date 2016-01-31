4ac55d1d14bd8108d3002c43e3d5fcde317dc0f0
switchkiller <emailbarung@gmail.com>
2016-01-31 14:32:56 +0530

    Ambigious soln dp

--- domains/codechef/bin/DP/lcs.cpp
+++ domains/codechef/bin/DP/lcs.cpp
@@ -2,23 +2,33 @@
 //BU approach
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 1000
+#define MAX 100
 #define REP(a,b,i) for (int i = (a); i <= (b); i++)
 #define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
-// char findLcs(char str1[], char str2[], int **lcs,int a, int b){
-//   if (lcs[a][b] == 0)
-//     return;
-//   else if (str1[a-1] == str2[b-1])
-//     findLcs(str1, str2, lcs, a-1, b-1);
-//   else if (lcs[i-1][j] > lcs[i][j-1])
-//     findLcs(str1, str2, lcs, a-1, b);
-//   else
-//     findLcs(str1, str2, lcs, a, b-1);
-//
-// }
+int n, a, b;
+int lcs[MAX][MAX];
+char *str1, *str2;
+string getLCS(){
+  string ret;
+  stack<char> s;
+  int ii = a, jj = b;
+  while (ii != 0 && jj != 0){
+    if (str1[ii-1] == str2[jj-1]){
+      s.push(str1[ii-1]);
+      ii--; jj--;
+    }
+    else if (lcs[ii-1][jj] > lcs[ii][jj-1]) ii--;
+    else jj--;
+  }
+  while (!s.empty()){
+    ret += s.top();
+    s.pop();
+  }
+  return ret;
+}
+
 
 int lcs_len(char str1[], char str2[], int a, int b){
-  int lcs[a+1][b+1];
   REP(0,a,i)
     lcs[i][0] = 0;
   REP(0,b,i)
@@ -35,16 +45,15 @@ int lcs_len(char str1[], char str2[], int a, int b){
 }
 
 int main(){
-  int n, a, b, len;
-  char *str1, *str2;
   scanf("%d\n", &n);
   while (n--){
-    scanf("%d %d\n", &a, &b);
+    memset(lcs, 0, sizeof lcs);
+    cin >> a >> b;
     str1 = new char[a];
     str2 = new char[b];
     cin >> str1 >> str2;
-    len = lcs_len(str1, str2, a, b);
-    printf("%d\n", len);
+    lcs_len(str1, str2, a, b);
+    cout << getLCS();
   }
   return 0;
 }
